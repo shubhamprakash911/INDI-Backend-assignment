7cc62376f5a518181a2581ed96235b9af533636b
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-10-06 11:20:07 +0530

    match password and hashing password while stroring password to db functionality implemented

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+});
+
+// match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// to hash the password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userSchema);
