f1f78dc9ac4a573750ecec1b45631a1a98900cb9
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-10-06 12:23:46 +0530

    authentication and authorization middleware created

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./asynHanler");
+
+// User must be authenticated
+const authenticate = asyncHandler(async (req, res, next) => {
+  // Read JWT from the 'jwt' cookie
+  const { token } = req.cookies;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+// User must be an admin
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+module.exports = { authenticate, admin };
