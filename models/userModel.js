77cbadec8a0bbcd427f579fea271024f461b6447
Shubham Prakash <shubhamprakash9112000@gmail.com>
2023-10-06 12:25:57 +0530

    isAdmin field added to user model

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 });
 
 // match user entered password to hashed password in database
